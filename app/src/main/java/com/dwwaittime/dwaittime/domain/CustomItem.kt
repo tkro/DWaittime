e6c5a320424e440f2e0ca42ba18593d2a27f3518
tkro <tkro.suga@gmaio.com>
2018-06-06 22:12:14 +0900

    ExpandableListView + Databindingを実装

--- app/src/main/java/com/dwwaittime/dwaittime/domain/CustomItem.kt
+++ app/src/main/java/com/dwwaittime/dwaittime/domain/CustomItem.kt
@@ -1,9 +1,10 @@
 package com.dwwaittime.dwaittime.domain
 
-public class CustomItem(var id: String,
+public class CustomItem(
+                        var id: String,
                         var name: String,
                         var operatingStatus: String,
-                        var time: Int,
+                        var time: String,
                         var fsStatus: String,
                         var updateTime: String
 )
